8dfd4caa89183a73c5fdb4694b17d6c005e6e0b7
surajrai-code <surajraieng>
2023-03-02 13:47:09 +0530

    add this

---
+++ src/ExpanseItem.js
@@ -0,0 +1,10 @@
+function expanseitem(){
+    return (<div>expanseitem
+        
+        <p>Food rs 20</p>
+        <p>petrol rs 100</p>
+        <p>movie rs 200</p>
+
+    </div>);
+}
+export default expanseitem;
