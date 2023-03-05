4845b72c0d3e6d76064903629b40a6091dad0889
surajrai-code <surajraieng>
2023-03-05 10:04:34 +0530

    delete item  function

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import ExpanseItem from './component/Expnses/ExpanseItem';
 const App=()=>{
       const expanseItem=[
         {
+          id: 'a',
       expanseDate:new Date(2023,2,3),
       expanseTitle:'bike',
       expanseAmount:23400,
       locationOfExpenditure:'patna' 
         },
         {
-          expanseDate:new Date(2023,2,2),
+          id: 'b',         
+           expanseDate:new Date(2023,2,2),
           expanseTitle:'car',
           expanseAmount:2347533,
           locationOfExpenditure:'chennai' 
             },
             {
+              id:'c',
               expanseDate:new Date(2023,2,6),
               expanseTitle:'mobile',
               expanseAmount:23400,
               locationOfExpenditure:'mumbai' 
                 },
                 {
+                  id:'d',
                   expanseDate:new Date(2023,2,7),
                   expanseTitle:'laptop',
                   expanseAmount:234786,
--- src/component/Expnses/Expance.js
+++ src/component/Expnses/Expance.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ExpanseDate from './ExpanseDate';
 import ExpanseDetails from './ExpanseDetails';
+import ExpanseItem from './ExpanseItem';
 const Expance=(props)=>{
-    console.log(props)
+  const [expanseItem, setList] = React.useState(props.expanseItem);
+
+  const DeleteItem=(id)=>{
+
+    const newList = expanseItem.filter((props) => props.id !== id);
+
+    setList(newList);
+  }
+    
     return(
            <div className='expense-item'>
          <ExpanseDate expanseDate={props.expanse.expanseDate}/>
           <ExpanseDetails expanseAmount={props.expanse.expanseAmount}/>
           <ExpanseDetails expanseTitle={props.expanse.expanseTitle}/>
           <ExpanseDetails locationOfExpenditure={props.expanse.locationOfExpenditure}/>
+          <button type='button' onClick={()=>DeleteItem(props.id)}>Delete Expance</button>
       </div>
     );
 }
